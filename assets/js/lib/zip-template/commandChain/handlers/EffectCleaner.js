e5a5b50735f94be03aa7374bde9a1bec7a546e69
Mathieu Bouvet <mathieubouvet42@gmail.com>
2021-05-14 00:11:36 +0200

    Check for callability

--- assets/js/lib/zip-template/commandChain/handlers/EffectCleaner.js
+++ assets/js/lib/zip-template/commandChain/handlers/EffectCleaner.js
@@ -1,9 +1,12 @@
 import { BaseHandler } from "../BaseHandler.js";
+import { isCallable } from "../../helpers/typeChecker.js";
 
 export class EffectCleaner extends BaseHandler {
   handle(request) {
     const { effectData } = request;
-    effectData.forEach(clean => clean());
+    effectData.forEach(clean => {
+      isCallable(clean) && clean();
+    });
     request.effectData = [];
     return request;
   }
