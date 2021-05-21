a8a012fd9caa6ab925a450808ee102945b8ec096
Mathieu Bouvet <mathieubouvet42@gmail.com>
2021-05-21 17:03:53 +0200

    fix path

--- src/dataLayer.js
+++ src/dataLayer.js
@@ -1,4 +1,4 @@
-const possibleUrls = ["assets/data.json", "../assets/data.json"];
+const possibleUrls = ["./assets/data.json", "../assets/data.json"];
 
 const requests = possibleUrls.map(url =>
   fetch(url).then(response => {
